eed4308598c61d50e17682459366f769a714302e
Gedas Bertasius <gedas@fb.com>
2019-12-06 16:23:03 -0500

    first commit

--- lib/models/pose_hrnet.py
+++ lib/models/pose_hrnet.py
@@ -1,4 +1,4 @@
-# Copyright 2018-present, Facebook, Inc.
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
